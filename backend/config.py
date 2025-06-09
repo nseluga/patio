847dccb657ed5f2781dd078052161bc228a6622c
Nate <nseluga@g.hmc.edu>
2025-06-09 13:04:45 -0400

    switched database url to transaction pooler

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@
 import os
 
 # Set the PostgreSQL database connection string (using default db name 'patio')
-DATABASE_URL = "postgresql://postgres:patiobestspotat5cs@db.dwnntzbzadiytktiwena.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres.dwnntzbzadiytktiwena:patiobestspotat5cs@aws-0-us-east-2.pooler.supabase.com:6543/postgres"
 
 # Set the secret key for JWT; fallback to default if not set in environment
 SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
