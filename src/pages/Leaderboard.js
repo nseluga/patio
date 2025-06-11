54597da2b3ea71f41d88f69c4731e763c41ac842
Logan Fowler <loganfowler@Logans-MacBook-Air-225.local>
2025-06-10 18:40:39 -0700

    Restored after I accidentally deleted everything

---
+++ src/pages/Leaderboard.js
@@ -0,0 +1,52 @@
+import BottomNav from '../components/BottomNav';
+// import { useState, useEffect } from 'react';
+import './Leaderboard.css';
+
+export default function Leaderboard() { // For Mike: Fix this after backend frontend connection works
+  const players = [
+    { name: 'Nate', caps: 300 },
+    { name: 'Drake', caps: 250 },
+    { name: 'Mike', caps: 180 },
+    { name: 'Skib', caps: 150 },
+  ];
+  // const [players, setPlayers] = useState([]);
+  // useEffect(() => {
+  //   fetch('http://localhost:5001/leaderboard')
+  //     .then(res => {
+  //       console.log("Raw response:", res);
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       console.log('Fetched leaderboard:', data);
+  //       setPlayers(data);
+  //     })
+  //     .catch(err => {
+  //       console.error('Failed to fetch leaderboard:', err);
+  //       alert('Failed to load leaderboard. Check console for details.');
+  //     });
+  // }, []);
+    return (
+      <div className="leaderboard-page">
+        <BottomNav />
+
+        <div className="leaderboard-title-wrapper">
+          <h2>LEADERBOARD</h2>
+        </div>
+  
+        <div className="leaderboard-list">
+          {players.map((player, index) => (
+            <div key={index} className="leaderboard-entry">
+              <span className={index === 1 || index === 2 ? "top-rank-symbol" : "below-top3-symbol"}>
+                {index === 0 && '🥇'}
+                {index === 1 && '🥈'}
+                {index === 2 && '🥉'}
+                {index > 2 && `#${index + 1}`}
+              </span>
+              <span>{player.name}</span>
+              <span>{player.caps} caps</span>
+            </div>
+          ))}
+        </div>
+    </div>
+  );
+}
